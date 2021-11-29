337bca7b3916557c85103a8daf0f3f3f71ab4373
Dominykas Usevicius <dominykas.usevicius@gmail.com>
2021-11-29 11:42:46 +0200

    hmtl and js change2

--- week10/week10d1git/naujasscript.js
+++ week10/week10d1git/naujasscript.js
@@ -1,3 +1,9 @@
 console.log("naujas script")
 const randomNumber = document.querySelector(".randomNumber")
 const button = document.querySelector("button")
+
+button.onclick = generateNumber
+
+function generateNumber() {
+    let randNumb = Math.floor(Math.random()*100)
+}
