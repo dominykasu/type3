1200259ddbd0a668d6b21718355f22fb3e5a6125
dominykasu <dominykas.usevicius@gmail.com>
2022-01-11 14:27:06 +0200

    a

--- week16react/w16d2lineagearmor/src/App.js
+++ week16react/w16d2lineagearmor/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import './App.css';
+import Shop from "./components/Shop";
+import Inventory from "./components/Inventory";
+import context from "./context/context";
 
 function App() {
+
+  const allItems = [
+    {
+      draconic: {
+        armor: "https://lineage.pmfun.com/data/img/armor_t89_ul_i00.png",
+        helmet: "https://lineage.pmfun.com/data/img/armor_leather_helmet_i00_0.png",
+        gloves: "https://lineage.pmfun.com/data/img/armor_t89_g_i00_0.png",
+        boots: "https://lineage.pmfun.com/data/img/armor_t89_b_i00_0.png"
+      }
+      ,
+      arcana: {
+        armor: "https://lineage.pmfun.com/data/img/armor_t90_ul_i00_0.png",
+        helmet: "https://lineage.pmfun.com/data/img/armor_helmet_i00_0.png",
+        gloves: "https://lineage.pmfun.com/data/img/armor_t90_g_i00_0.png",
+        boots: "https://lineage.pmfun.com/data/img/armor_t90_b_i00_0.png"
+      }
+      ,
+      tallum: {
+        armor: "https://lineage.pmfun.com/data/img/armor_t77_ul_i00_0.png",
+        helmet: "https://lineage.pmfun.com/data/img/armor_helmet_i00_0.png",
+        gloves: "https://lineage.pmfun.com/data/img/armor_t77_g_i00_0.png",
+        boots: "https://lineage.pmfun.com/data/img/armor_t77_b_i00_0.png"
+      }
+      ,
+      majestic: {
+        armor: "https://lineage.pmfun.com/data/img/armor_t85_ul_i00_0.png",
+        helmet: "https://lineage.pmfun.com/data/img/armor_leather_helmet_i00_0.png",
+        gloves: "https://lineage.pmfun.com/data/img/armor_t85_g_i00_0.png",
+        boots: "https://lineage.pmfun.com/data/img/armor_t85_b_i00_0.png"
+      }
+    }
+  ]
+  console.log(allItems[0].draconic)
   return (
+      <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/shop">Shop</Link>
+      <Link to="/inventory">Inventory</Link>
     </div>
+        <Routes>
+          <Route path="/shop" element={<Shop/>}>
+          </Route>
+          <Route path="/inventory" element={<Inventory/>}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ week16react/w16d2lineagearmor/src/context/context.js
@@ -0,0 +1,3 @@
+import {useContext} from "react";
+const UserContext = createContext()
+export default useContext()
